1bc87e1b4172e497627ca98c39e3397f67e64464
Scott McPherson <scott_fl@me.com>
2017-08-25 10:48:44 -0400

    Init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { DocumentList } from 'test-library'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          <DocumentList />
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
